4ea20a1c8b38712f1617fafc94917513f971f7a2
Mateusz Poreba <mateusz.poreba@outlook.com>
2022-11-08 09:50:55 +0100

    wip

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const func1 = () => {
-  console.log("npm basic module func1");
+  console.log("npm basic module function one");
 }
 
  const func2 = () => {
-  console.log("npm basic module func2");
+  console.log("npm basic module function two");
 }
 
 module.exports = {func1, func2};
